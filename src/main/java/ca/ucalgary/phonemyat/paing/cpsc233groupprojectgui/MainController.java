be1c71c10431be7f9b529920d0952853b6cc45c0
phone <phonemyat.pmp2022@gmail.com>
2024-04-08 18:53:33 -0600

    Name Changes to the Files

--- src/main/java/ca/ucalgary/phonemyat/paing/cpsc233groupprojectgui/MainController.java
+++ src/main/java/ca/ucalgary/phonemyat/paing/cpsc233groupprojectgui/MainController.java
@@ -3,7 +3,7 @@ package ca.ucalgary.phonemyat.paing.cpsc233groupprojectgui;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 

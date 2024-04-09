39300d0b5feab603f243f9b54a8a76a1dbdfd8eb
TaniaRizwan <105510572+TaniaRizwan@users.noreply.github.com>
2024-04-08 22:01:31 -0600

    Created global alert object

--- src/main/java/ca/ucalgary/phonemyat/paing/cpsc233groupprojectgui/MainController.java
+++ src/main/java/ca/ucalgary/phonemyat/paing/cpsc233groupprojectgui/MainController.java
@@ -1,6 +1,7 @@
 package ca.ucalgary.phonemyat.paing.cpsc233groupprojectgui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 
 public class MainController {
@@ -8,6 +9,9 @@ public class MainController {
     // Status bar
     @FXML
     private Label statusLabel;
+
+    // Global Alert obj
+    Alert a = new Alert(Alert.AlertType.NONE);
 
 
     // Menu: Edit
